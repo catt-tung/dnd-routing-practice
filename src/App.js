3c013e47baa3e1b5768e034464a320a92a9cbca2
Catherine Tung <catt.tung@gmail.com>
2022-06-28 18:13:58 -0400

    Add components and files, render Search in landing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
+import SpellSearch from './components/SpellSearch/SpellSearch';
 
 function App() {
   return (
     <>
       <img className='App-logo' src={logo} alt="" />
       <h1>React Dungeons and Dragons Practice</h1>
-      
+      <Routes>
+        <Route path="/" element={<SpellSearch />}/>
+      </Routes>
     </>
   );
 }
---
+++ src/components/SpellSearch/SpellSearch.jsx
@@ -0,0 +1,7 @@
+const SpellSearch = () => {
+  return ( 
+    <h3>Search for Spells</h3>
+  );
+}
+
+export default SpellSearch;
