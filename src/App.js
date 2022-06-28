0989baf57aa38c04a246ee2deacba1ae280e6b86
Catherine Tung <catt.tung@gmail.com>
2022-06-28 17:54:42 -0400

    Clean up starter code and add some starting code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <img className='App-logo' src={logo} alt="" />
+      <h1>React Dungeons and Dragons Practice</h1>
+      
+    </>
+  );
+}
+
+export default App;
