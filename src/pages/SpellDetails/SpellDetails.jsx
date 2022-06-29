ca4577b62b15ed2a0486603499a7e6f48f26d058
Catherine Tung <catt.tung@gmail.com>
2022-06-29 18:06:56 -0400

    stub up spell details component/page

---
+++ src/pages/SpellDetails/SpellDetails.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const SpellDetails = (props) => {
+  const [spellDetails, setSpellDetails] = useState({})
+  
+  return (
+    <>
+      <h3>Spell Details!</h3>
+    </>
+  );
+}
+
+export default SpellDetails;
