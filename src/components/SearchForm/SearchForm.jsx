5bdd088e65cd9aec10dcf028b2c46fee73d8e7f4
Catherine Tung <catt.tung@gmail.com>
2022-06-29 17:55:23 -0400

    Submitting form data to API call

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
 
-const SearchForm = () => {
+const SearchForm = (props) => {
   const [formData, setFormData] = useState({query: ''})
 
   const handleChange = (evt) => {
     setFormData({ ...formData,[evt.target.name]: evt.target.value })
   }
 
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    props.handleSpellSearch(formData)
+  }
+
   return (
     <>
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input 
           name="query" 
           type="text" 
--- src/components/SpellSearch/SpellSearch.jsx
+++ src/components/SpellSearch/SpellSearch.jsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 import SearchForm from "../SearchForm/SearchForm";
+import { spellSearch } from "../../services/api-calls";
 
 
 const SpellSearch = () => {
   const [spells, setSpells] = useState([])
+
+  const handleSpellSearch = formData => {
+    spellSearch(formData)
+    .then(spellResults => {
+      setSpells(spellResults.results)
+    })
+  }
+
   return (
     <>
       <h3>Search for Spells</h3>
-      <SearchForm />
+      <SearchForm handleSpellSearch={handleSpellSearch} />
     </>
   );
 }
