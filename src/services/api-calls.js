07f750e7dbb7726582d81bae619bdc320a8717de
Catherine Tung <catt.tung@gmail.com>
2022-06-29 18:09:10 -0400

    Add function for getting spell details API call

--- src/services/api-calls.js
+++ src/services/api-calls.js
@@ -4,3 +4,8 @@ export function spellSearch(formData) {
   return fetch(`${baseUrl}spells/?name=${formData.query}`)
   .then(res => res.json())
 }
+
+export function getSpellDetails(spellName) {
+  return fetch(`${baseUrl}spells/${spellName}`)
+  .then(res => res.json())
+}
